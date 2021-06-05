448914e3161b24a5e54e14fa23e7b6e8fe16e9e2
pyr53540 <46209669+pyr53540@users.noreply.github.com>
2021-06-05 18:52:02 +0900

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,34 +1,7 @@
-<html>
-<!--210525 로그인(완) 닉네임(미완) 유저가 다운로드한 목록(미완)-->
-<head>
-         <!-- The surrounding HTML is left untouched by FirebaseUI.
-         Your app may use that space for branding, controls and other customizations.-->
-         <h1><p style="text-align:center;">Welcome to welvi store</p></h1>
-         <div id="firebaseui-auth-container"></div>
-         <div id="loader">Loading...</div>
-         
-         
-         <meta http-equiv="Permissions-Policy" content="interest-cohort=()"/>
-         <link rel="shortcut icon" href="#">
-         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-         <title>welvi store</title> 
-</head>
-
-<body>
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-analytics.js"></script>
-
-
-<!--Authentication-->         
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/ui/4.8.0/firebase-ui-auth.js"></script>
-<link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.8.0/firebase-ui-auth.css" />
-             
-         
-<script>
-         <!--initialize firebase-->
-         var config = {
+(fucntion() {
+ 
+         // Initialize Firebase
+         const config = {
          apiKey: "AIzaSyBFpJ_jHiLPpl4HZckHefuj4_XJxSQTvlg",
          authDomain: "opensw-opener.firebaseapp.com",
          databaseURL: "https://opensw-opener-default-rtdb.firebaseio.com",
@@ -39,53 +12,22 @@
          measurementId: "G-GCDBT9FVRL"
          };
          firebase.initializeApp(config);
-         firebase.analytics();
- 
-         
-         <!--Authentication-->
-         <!--Initialize the FirebaseUI Widget using Firebase.-->
-         var ui = new firebaseui.auth.AuthUI(firebase.auth());
-         
-         var uiConfig = {
-         callbacks: {
-                  signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-         // User successfully signed in.
-         // Return type determines whether we continue the redirect automatically
-         // or whether we leave that to developer to handle.
-                           return true;
-                  },
-                  uiShown: function() {
-         // The widget is rendered.
-         // Hide the loader.
-                           document.getElementById('loader').style.display = 'none';
-                  }
-         },
-         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-                  signInFlow: 'popup',
-                  signInSuccessUrl: 'store.html',
-                  signInOptions: [
-         // Leave the lines as is for the providers you want to offer your users.
-         //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-         //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-         //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-         //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-                           //firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                           {
-                                    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                                    requireDisplayName: true
-                           }
-         //firebase.auth.PhoneAuthProvider.PROVIDER_ID
-                  ],
-         // Terms of service url.
-                  //tosUrl: '<your-tos-url>',
-         // Privacy policy url.
-                  //privacyPolicyUrl: '<your-privacy-policy-url>'
-         };
-         
-         <!--The start method will wait until the DOM is loaded.-->
-         ui.start('#firebaseui-auth-container', uiConfig);       
-</script>
 
-</body>
-        
-</html>
+         // Get elements
+         const txtEmail = document.getElementById('txtEmail');
+         const txtPassword = document.getElementById('txtPassword');
+         const btnLogin = document.getElementById('btnLogin');
+         const btnSignUp = document.getElementById('btnSignUp');
+         const btnLogout = document.getElementById('btnLogout');
+
+         // Add login event
+         btnLogin.addEventListener('click', e=> {
+                  // Get email and pass
+                  const email = txtEmail.value;
+                  const pass =txtPassword.value;
+                  const auth = firebase.auth();
+                  //Sign in
+                  const promise = auth.signInWithEmailAndPassword(email.password);
+                  promise.catch(e => console.log(e.message));
+         
+}());
